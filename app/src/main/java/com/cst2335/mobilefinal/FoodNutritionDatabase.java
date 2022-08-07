3c860dc2661f728c37f2c7a30660efe7590d9c4c
Peiyuan <li000758@algonquinlive.com>
2022-08-07 17:55:49 -0400

    Peiyuan Finish

---
+++ app/src/main/java/com/cst2335/mobilefinal/FoodNutritionDatabase.java
@@ -0,0 +1,18 @@
+package com.cst2335.mobilefinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodNutritionDatabase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_nutri_database);
+    }
+}
